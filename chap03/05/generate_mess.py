d1b15006e4bf856f066fcdef42492d0c7080d5f0
kanehiro <kanehiro@gmail.com>
2024-08-26 15:29:53 +0900

    Add files via upload

---
+++ chap03/05/generate_mess.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()   #上位フォルダにある.envファイルも読んでくれる
+client = OpenAI()
+
+
+
+# MODEL_ENGINE = "gpt-3.5-turbo"
+MODEL_ENGINE = "gpt-4o-mini"
+ROLE_SYSTEM = {"role": "system", "content":"あなたは優秀な教授です"} 
+
+messages = []
+messages.append(ROLE_SYSTEM)
+
+def to_dict(obj):
+    return {
+        "content": obj.content,
+        "role": obj.role,
+    }
+
+def generate_chat_completion(user_input=""):
+    messages.append({"role": "user", "content": user_input})
+    response = client.chat.completions.create(
+        model=MODEL_ENGINE,
+        messages=messages,
+        temperature=0.8,
+        max_tokens=100,
+    )
+    message = response.choices[0].message
+    messages.append(to_dict(message))
+    return message.content
+    # print("ボット: " + message.content.replace("\n", ""))
+
