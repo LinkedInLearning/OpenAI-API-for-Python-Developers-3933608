3663ff7bf2560a4f6580e2fae37f7468c405c73a
kanehiro <kanehiro@gmail.com>
2024-10-01 02:31:42 -0700

    Update app.py

--- chap05/02/app.py
+++ chap05/02/app.py
@@ -11,6 +11,6 @@ with st.form("user_form", clear_on_submit=True):
 if submit_button:
     with st.spinner("画像生成中..."):
         image = generate_image(user_input)
-        st.image(image, use_column_width=True)  
+        # st.image(image, use_column_width=True)  
         saved_image = downloadFile(user_input, image)
         st.success("imgフォルダに画像を保存しました")
